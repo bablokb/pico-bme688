65ab26a24cb672ce4f8eca508f45bda43b088a73
Bernhard Bablok <bablokb@gmx.de>
2021-12-28 12:43:45 +0100

    fixed i2c_read/i2c_write

--- lib/src/bme68x_platform.c
+++ lib/src/bme68x_platform.c
@@ -7,12 +7,11 @@
 // --------------------------------------------------------------------------
 
 #include <stdio.h>
+#include <string.h>
 #include "bme68x_platform.h"
 #include "bme68x.h"
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-
-static uint8_t _bme68x_dev_addr = BME68X_I2C_ADDR_HIGH;
 
 // --------------------------------------------------------------------------
 // return timestamp (pico: elapsed ms since boot)
@@ -35,9 +34,10 @@ void platform_delay_us(uint32_t period, void *intf_ptr) {
 //
 
 BME68X_INTF_RET_TYPE platform_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
-                                              uint32_t len, void *intf_ptr) {
-  uint8_t dev_addr = *(uint8_t*)intf_ptr;
-  return len != i2c_read_blocking(BME68X_PORT,dev_addr,reg_data,(size_t) len,true);  // true - finished with bus
+                                       uint32_t len, void *intf_ptr) {
+  i2c_write_blocking(BME68X_PORT,BME68X_ADDR,&reg_addr,1,true);
+  i2c_read_blocking(BME68X_PORT,BME68X_ADDR,reg_data,len,false);
+  return 0;
 }
 
 // --------------------------------------------------------------------------
@@ -45,9 +45,13 @@ BME68X_INTF_RET_TYPE platform_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
 //
 
 BME68X_INTF_RET_TYPE platform_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
-                                                  uint32_t len, void *intf_ptr) {
-  uint8_t dev_addr = *(uint8_t*)intf_ptr;
-  return len != i2c_write_blocking(BME68X_PORT,dev_addr,reg_data,(size_t)len,true);
+                                        uint32_t len, void *intf_ptr) {
+  uint8_t buf[len+1];
+  buf[0] = reg_addr;
+  memcpy(buf+1,reg_data,len);
+
+  i2c_write_blocking(BME68X_PORT,BME68X_ADDR,buf,(uint8_t) len+1,false);
+  return 0;
 }
 
 // --------------------------------------------------------------------------
@@ -57,19 +61,19 @@ BME68X_INTF_RET_TYPE platform_i2c_write(uint8_t reg_addr, const uint8_t *reg_dat
 int8_t platform_interface_init(struct bme68x_dev *bme) {
   int8_t rslt = BME68X_OK;
   if (bme) {
-     // I2C Initialisation. Using it at 400Khz.
+    // I2C Initialisation. Using it at 400Khz.
     i2c_init(BME68X_PORT,400*1000);
     gpio_set_function(BME68X_SDA,GPIO_FUNC_I2C);
     gpio_set_function(BME68X_SCL,GPIO_FUNC_I2C);
+#ifdef BME68X_I2C_PU
     gpio_pull_up(BME68X_SDA);
     gpio_pull_up(BME68X_SCL);
-
+#endif
     // device-structure init
     bme->read     = platform_i2c_read;
     bme->write    = platform_i2c_write;
     bme->intf     = BME68X_I2C_INTF;
     bme->delay_us = platform_delay_us;
-    bme->intf_ptr = &_bme68x_dev_addr;
     bme->amb_temp = 20;              // initial ambient temperature in deg C
   } else {
     rslt = BME68X_E_NULL_PTR;
@@ -108,4 +112,3 @@ void bme68x_print_result(const char api_name[], int8_t rslt) {
     break;
   }
 }
-
